6bdb13b8a9610b2ed36dfa0addad28fc325c539f
Fabrizio99 <avbar357159@gmail.com>
2019-09-03 00:29:25 -0500

    script done

---
+++ script.js
@@ -0,0 +1,13 @@
+const filterInput = document.getElementById('filterInput')
+
+filterInput.addEventListener('keyup',(keyboardEvent)=>{
+    const filterValue = keyboardEvent.target.value.toUpperCase()
+    const ul = document.getElementById('names')
+    const li = Array.from(ul.querySelectorAll('li.collection-item'))
+    
+    li.forEach(nameTag => {
+        let name = nameTag.getElementsByTagName('a')[0]
+        const coincide = name.textContent.toUpperCase().includes(filterValue)
+        nameTag.style.display = coincide? '':'none'
+    });
+})
